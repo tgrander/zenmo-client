b16fb9f668146335084b16fa8139c9a8cced4875
Trey Granderson <treygranderson@gmail.com>
2017-06-22 18:41:14 -0400

    add styles-components folder

---
+++ src/styled_components/text/big-header.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const BigHeader = styled.div`
+  font-size: 80px;
+  color: black;
+  letter-spacing: 40px;
+`;
+
+export default BigHeader;
---
+++ src/styled_components/text/caption.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Caption = styled.div`
+  font-size: 20px;
+  color: black;
+`;
+
+export default Caption;
