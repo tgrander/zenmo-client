ec93ca0736dca1d51632ff7b836d8a6bc27de793
Trey Granderson <treygranderson@gmail.com>
2017-06-27 14:18:20 -0400

    [fix] Big preventing redux form container from connecting with form component

--- src/modules/dashboard/components/Overview.js
+++ src/modules/dashboard/components/Overview.js
@@ -3,24 +3,25 @@ import { Field } from 'redux-form';
 
 import InputField from './InputField';
 
-function Overview() {
-  return (
-    <form>
-      <Field
-        name="checkings"
-        type="number"
-        component={InputField}
-        label="Checkings" />
-      <Field
-        name="savings"
-        type="number"
-        component={InputField}
-        label="Savings" />
-      <Field
-        name="monthlyIncome"
-        type="number"
-        component={InputField}
-        label="Monthly Income" />
-    </form>
-  )
-}
+const Overview = () => (
+  <form>
+    <Field
+      name="checkings"
+      type="number"
+      component={InputField}
+      label="Checkings" />
+    <Field
+      name="savings"
+      type="number"
+      component={InputField}
+      label="Savings" />
+    <Field
+      name="monthlyIncome"
+      type="number"
+      component={InputField}
+      label="Monthly Income" />
+  </form>
+
+)
+
+export default Overview;
