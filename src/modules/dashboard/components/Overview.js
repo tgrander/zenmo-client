3f8cd30916471d2ead00e9b567f37b6e8fcf43d8
Trey Granderson <treygranderson@gmail.com>
2017-06-27 15:00:25 -0400

    create prototype / mockup for trial balance

--- src/modules/dashboard/components/Overview.js
+++ src/modules/dashboard/components/Overview.js
@@ -5,13 +5,34 @@ import InputField from './InputField';
 
 import '../styles/overview.css';
 
-const Overview = () => (
+const Overview = ({
+  totalAssets,
+  totalExpenses,
+  bottomLine
+}) => (
   <section className="overview">
+
     <div className="labels">
       <div className="label">Checkings</div>
       <div className="label">Savings</div>
       <div className="label">Monthly Income</div>
+
+      <div className="label total">Total Assets</div>
+
+      <div className="label">Rent</div>
+      <div className="label">Bootcamp Loans</div>
+      <div className="label">Student Loans</div>
+      <div className="label">Monthly Expenses</div>
+
+      <div className="label total">Total Expenses</div>
+      <div className="label total">Net Income</div>
+
+      <div className="label">Put Towards Savings</div>
+      <div className="label">Invest In Stocks</div>
+
+      <div className="bottom-line">Leftover for Fun</div>
     </div>
+
     <form className="overview-amounts">
       <Field
         name="checkings"
@@ -28,7 +49,47 @@ const Overview = () => (
         type="number"
         component={InputField}
         label="Monthly Income" />
+
+      <div className="total">{totalAssets ? totalAssets : 0.00}</div>
+
+      <Field
+        name="monthlyIncome"
+        type="number"
+        component={InputField}
+        label="Rent" />
+      <Field
+        name="monthlyIncome"
+        type="number"
+        component={InputField}
+        label="Bootcamp Loans" />
+      <Field
+        name="monthlyIncome"
+        type="number"
+        component={InputField}
+        label="Student Loans" />
+      <Field
+        name="monthlyIncome"
+        type="number"
+        component={InputField}
+        label="Monthly Expenses" />
+
+      <div className="total">{totalExpenses ? totalExpenses : 0.00}</div>
+      <div className="total">{totalExpenses ? totalExpenses : 0.00}</div>
+
+      <Field
+        name="monthlyIncome"
+        type="number"
+        component={InputField}
+        label="Put Towards Savings" />
+      <Field
+        name="monthlyIncome"
+        type="number"
+        component={InputField}
+        label="Invest in Stocks" />
+
+      <div className="bottom-line">{bottomLine ? bottomLine : 0.00}</div>
     </form>
+
   </section>
 )
 
