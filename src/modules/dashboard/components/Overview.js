4e38db7e9868f4479a0467b3535f8e831733cd7c
Trey Granderson <treygranderson@gmail.com>
2017-06-27 14:36:12 -0400

    overview stylesheet

--- src/modules/dashboard/components/Overview.js
+++ src/modules/dashboard/components/Overview.js
@@ -4,24 +4,28 @@ import { Field } from 'redux-form';
 import InputField from './InputField';
 
 const Overview = () => (
-  <form>
-    <Field
-      name="checkings"
-      type="number"
-      component={InputField}
-      label="Checkings" />
-    <Field
-      name="savings"
-      type="number"
-      component={InputField}
-      label="Savings" />
-    <Field
-      name="monthlyIncome"
-      type="number"
-      component={InputField}
-      label="Monthly Income" />
-  </form>
+  <section className="overview">
+    <div>
 
+    </div>
+    <form>
+      <Field
+        name="checkings"
+        type="number"
+        component={InputField}
+        label="Checkings" />
+      <Field
+        name="savings"
+        type="number"
+        component={InputField}
+        label="Savings" />
+      <Field
+        name="monthlyIncome"
+        type="number"
+        component={InputField}
+        label="Monthly Income" />
+    </form>
+  </section>
 )
 
 export default Overview;
