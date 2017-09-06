f5a683108b5261838ca995cac5baa97bea589b72
Trey Granderson <treygranderson@gmail.com>
2017-09-06 08:44:46 -0400

    able to create item in DB

--- src/modules/plaid/plaid-link.js
+++ src/modules/plaid/plaid-link.js
@@ -8,34 +8,44 @@ function PlaidLinkConnection() {
 
   const PUBLIC_TOKEN = 'b41ccce2d4bf2d77e8b21c4ff67fef'
 
-  const userId = auth.currentUser.uid
+  const userId = auth.currentUser ? auth.currentUser.uid : null
+
+  console.log('userId', userId);
 
   const handleOnSuccess = (public_token, metadata) => {
 
-    axios.post('/plaid/create-item', {public_token, userId})
+    axios.post('/plaid/create-item', { public_token, userId })
       .then(res => console.log(res))
       .catch(err => console.error(err))
   }
 
   const getTransactions = () => {
-    axios.post('/plaid/transactions')
+    axios.get('/plaid/transactions')
       .then(res => console.log(res))
   }
+
+  const addItem = () => {
+
+      axios.post('/plaid/add-item', { userId })
+          .then(res => console.log(res))
+  }
+
 
   const linkHandler = window.Plaid.create({
     apiVersion: 'v2',
     env: 'development',
-    clientName: 'Zenmo - Financial Nirvana',
+    clientName: 'Zenmo',
     key: PUBLIC_TOKEN,
     product: ['transactions', 'connect'],
     selectAccount: true,
-    onSuccess: handleOnSuccess,
+    onSuccess: handleOnSuccess
   })
 
   const openLink = () => linkHandler.open()
 
     return (
         <div style={{display: 'flex', flexDirection: 'column'}}>
+
             <Button onClick={openLink}>
                 Create A New Item
             </Button>
@@ -43,6 +53,11 @@ function PlaidLinkConnection() {
             <Button onClick={getTransactions}>
                 Get Transactions
             </Button>
+
+            <Button onClick={() => addItem()}>
+                Add Item to Database
+            </Button>
+
         </div>
     )
 }
